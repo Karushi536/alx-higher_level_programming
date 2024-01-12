e26bb430eeeedadf4f6fd3fdc79942699dc75930
Abigael Karushi <karushiabigael@gmail.com>
2024-01-12 08:05:31 -0800

    Fixed white spaces

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
-def uniq_add(my_list=[]))
-    """Adds all unique integers in a list."""
-    unique_sum = sum(set(my_list))
-    return unique_sum
+def uniq_add(my_list=[]):
+    set_list = set(my_list)
+    sum_no = 0
+    for no in set_list:
+        sum_no += no
+    return sum_no
