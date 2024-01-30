402e5358298056cbc4bb7b3b91e1289b58d30f95
Karushi536 <karushiabigael@gmail.com>
2024-01-30 23:27:24 +0300

    code fixing

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,39 +1,15 @@
 #!/usr/bin/python3
-"""This module provides a function to add two integers."""
+''' Integer addition in test driven development'''
 
 
 def add_integer(a, b=98):
-    """
-    Add two integers.
-
-    Args:
-        a (int or float): The first integer or float.
-        b (int or float, optional): The second integer or float. Defaults to 98.
-
-    Returns:
-        int: The addition of a and b as integers.
-
+    """Return the integer addition of a and b.
+    Float arguments are typecasted to ints before addition is performed.
     Raises:
-        TypeError: If a or b are not integers or floats.
-
+        TypeError: If either of a or b is a non-integer and non-float.
     """
-    if not isinstance(a, (int, float)):
+    if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+    if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
-    return int(a) + int(b)
-
-
-if __name__ == "__main__":
-    print(add_integer(1, 2))
-    print(add_integer(100, -2))
-    print(add_integer(2))
-    print(add_integer(100.3, -2))
-    try:
-        print(add_integer(4, "School"))
-    except Exception as e:
-        print(e)
-    try:
-        print(add_integer(None))
-    except Exception as e:
-        print(e)
+    return (int(a) + int(b))
