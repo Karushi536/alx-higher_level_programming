3122dac28e59c4a404f6fa6e25712abb14612b35
Karushi536 <karushiabigael@gmail.com>
2024-05-22 23:06:28 +0300

    fixing errors

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,34 @@
 #!/usr/bin/python3
 """
-Lists all states from the database hbtn_0e_0_usa
+Script that lists all states from the database hbtn_0e_0_usa
 """
-import sys
+
 import MySQLdb
+from sys import argv
 
-if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3], port=3306)
+def list_states(username, password, database):
+    """Lists all states from the database hbtn_0e_0_usa"""
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
 
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states;")
-    states = cur.fetchall()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    states = cursor.fetchall()
 
     for state in states:
         print(state)
+
+    cursor.close()
+    db.close()
+
+if __name__ == "__main__":
+    if len(argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(argv[0]))
+    else:
+        username, password, database = argv[1], argv[2], argv[3]
+        list_states(username, password, database)
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,21 +1,35 @@
 #!/usr/bin/python3
 """
-Lists all states with a name starting with N
+Script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa
 """
-import sys
+
 import MySQLdb
+from sys import argv
 
-if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3], port=3306)
+def list_states_starting_with_N(username, password, database):
+    """Lists all states starting with N from the database hbtn_0e_0_usa"""
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
 
-    cur = db.cursor()
-    cur.execute("SELECT * \
-    FROM states \
-    WHERE CONVERT(`name` USING Latin1) \
-    COLLATE Latin1_General_CS \
-    LIKE 'N%';")
-    states = cur.fetchall()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    states = cursor.fetchall()
 
     for state in states:
         print(state)
+
+    cursor.close()
+    db.close()
+
+if __name__ == "__main__":
+    if len(argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(argv[0]))
+    else:
+        username, password, database = argv[1], argv[2], argv[3]
+        list_states_starting_with_N(username, password, database)
